ee1db31a28ca9519f679729f186f1dc68739f3c9
Winson <winosn>
2025-08-25 16:12:57 +0800

    Initial Commit

---
+++ IOS LESSON 7/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IOS LESSON 7
+//
+//  Created by Chan Poh Lian on 25/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IOS LESSON 7/IOS_LESSON_7App.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_LESSON_7App.swift
+//  IOS LESSON 7
+//
+//  Created by Chan Poh Lian on 25/8/25.
+//
+
+import SwiftUI
+
+@main
+struct IOS_LESSON_7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
